4cbbeabd972360f5d6eb7da3dbfe5ce308af0768
Ray Alva <rayalva407@gmail.com>
2020-04-13 17:00:11 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -27,7 +27,7 @@ class MusicLibraryController
     
     def list_artists
       Artist.all.sort {|a, b| a.name <=> b.name}.each_with_index do |song, i|
-        puts "#{i + 1}. #song.artist.name"
+        puts "#{i + 1}. #{song.artist.name}"
       end
     end
     
