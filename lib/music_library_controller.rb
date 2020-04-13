8699fa04d5622ecf520b412e69c4ac9154ffb6c2
Ray Alva <rayalva407@gmail.com>
2020-04-13 18:26:15 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,7 +25,10 @@ class MusicLibraryController
           list_artists
         when "list genres"
           list_genres
-        when 
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
         end
     end
       
