53ea2943d791d2b3aa656e688df262f0f0af6b1d
Ray Alva <rayalva407@gmail.com>
2020-04-13 18:25:32 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,6 +21,11 @@ class MusicLibraryController
       case input
         when "list songs"
           list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when 
         end
     end
       
